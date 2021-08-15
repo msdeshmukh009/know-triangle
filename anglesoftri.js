a26f54cd94c45797bd93fc28ceada3d3699ad472
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-08-15 14:48:19 +0530

    updated: anglesoftri.html

--- anglesoftri.js
+++ anglesoftri.js
@@ -1,18 +1,19 @@
 var inputAngles = document.querySelectorAll(".input");
 var outputDiv = document.querySelector("#isTriangle");
-var checkBtn = document.querySelectorAll("#button")
+var checkBtn = document.querySelector("#button");
+var form =document.querySelector("#input-form");
 
-checkBtn.addEventListener("click",angleHandler);
-
-function angleHandler(){
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
     var firstAngle = Number(inputAngles[0].value);
     var secondAngle = Number(inputAngles[1].value);
     var ThirdAngle = Number(inputAngles[2].value);
-
-    if(firstAngle+secondAngle+ThirdAngle===180){
-        console.log("yes")
+    var sum = firstAngle+secondAngle+ThirdAngle;
+    
+    if(sum===180){
+        outputDiv.innerText = "Yup!! this will work."
     }else{
-        console.log("no")
+        outputDiv.innerText = "nahh!! this is not feeling right."
     }
+})
 
-}
